b1283cce3078fee5b585051c84836581b0b1c677
systemWaska <sistemaswaska@gmail.com>
2025-12-24 14:28:43 -0500

    Update mis-tickets.js

--- js/mis-tickets.js
+++ js/mis-tickets.js
@@ -1,4 +1,4 @@
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyXdV2cveu1ECWhi-3HMODJxKLIrEHmk1dA-15vAq1N8X-X_PRIR8t43i6-ulc_J5Dwxg/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzrRHvRztFxPDWD7evVT86hXEAvPoTCwWVgMQ2ROYMLGqoFHavCdwQTWRKYyCJHutf5Eg/exec";
 
 document.addEventListener('DOMContentLoaded', () => {
     mostrarUltimos3();
