3f3588157dbf442928f6ae91ee8e54f5c12f9cb2
systemWaska <sistemaswaska@gmail.com>
2025-12-24 14:58:43 -0500

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,5 @@
+// CENTRALIZACIÓN DE LA URL DEL BACKEND
+const CONFIG = {
+    // Pega aquí tu URL de implementación más reciente
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyXdV2cveu1ECWhi-3HMODJxKLIrEHmk1dA-15vAq1N8X-X_PRIR8t43i6-ulc_J5Dwxg/exec"
+};
