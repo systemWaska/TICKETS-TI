a815c86f29cd85420c9bbf7ffb156bc366b30b0c
systemWaska <sistemaswaska@gmail.com>
2025-12-29 07:06:19 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // CENTRALIZACIÓN DE LA URL DEL BACKEND
 const CONFIG = {
     // Pega aquí tu URL de implementación más reciente
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyXdV2cveu1ECWhi-3HMODJxKLIrEHmk1dA-15vAq1N8X-X_PRIR8t43i6-ulc_J5Dwxg/exec"
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbyfef4NbXuBwn__PkPDBEkZNsP1RXwGldXMLPy1Gptr8s-HaIh0gPqJQMDogSzmcWM9VA/exec"
 };
